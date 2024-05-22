3747ca03ae9899565976a5faad3a71d6c6168d7b
Kojo Acquah <k.acquah@contentbloom.com>
2024-05-22 03:49:15 -0300

    tic-tac-toe project

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSquareSelect(rowIndex, colIndex) {
+    setGameBoard((previousGameBoard) => {
+      const updatedBoard = [
+        ...previousGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+
+    onSelectSquare();
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSquareSelect(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
---
+++ src/components/Player.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol, isActive }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" value={playerName} required onChange={handleChange} />
+    );
+  }
+
+  return (
+    <>
+      <li className={isActive ? "active" : undefined}>
+        <span className="player">
+          {editablePlayerName}
+          <span className="player-symbol">{symbol}</span>
+          {
+            <button onClick={handleEditClick}>
+              {isEditing ? "Save" : "Edit"}
+            </button>
+          }
+        </span>
+      </li>
+    </>
+  );
+}
