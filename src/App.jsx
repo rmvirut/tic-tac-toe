d6c17a847e3d7b6d2b87e7d6fe6cfbd98e139445
Kojo Acquah <k.acquah@contentbloom.com>
2024-05-22 12:58:31 -0300

    cleaned up code

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import GameOver from "./components/GameOver";
 
-const initialGameBoard = [
+const PLAYERS = {
+  X: "Player 1",
+  O: "Player 2",
+};
+
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
@@ -21,7 +26,22 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
-function determineWinner(gameBoard, gameTurns) {
+function deriveGameBoard(gameTurns) {
+  let gameBoard = [...INITIAL_GAME_BOARD.map((array) => [...array])];
+
+  // iterate over all the turns (won't run if no turns)
+  for (const turn of gameTurns) {
+    // extract info from the turn object
+    const { square, player } = turn;
+    const { row, col } = square;
+    // update the gameboard
+    gameBoard[row][col] = player;
+  }
+
+  return gameBoard;
+}
+
+function deriveWinner(gameBoard, players) {
   let results = false;
   for (
     let row = 0, col = 0;
@@ -34,7 +54,7 @@ function determineWinner(gameBoard, gameTurns) {
       gameBoard[row][0] == gameBoard[row][1] &&
       gameBoard[row][0] == gameBoard[row][2]
     ) {
-      return true;
+      return players[gameBoard[row][0]];
     }
 
     // check for winning column
@@ -43,47 +63,32 @@ function determineWinner(gameBoard, gameTurns) {
       gameBoard[0][col] == gameBoard[1][col] &&
       gameBoard[0][col] == gameBoard[2][col]
     ) {
-      return true;
+      return players[gameBoard[0][col]];
     }
   }
 
   // check diagonal
   if (gameBoard[1][1]) {
-    return (
+    if (
       (gameBoard[1][1] == gameBoard[0][0] &&
         gameBoard[1][1] == gameBoard[2][2]) ||
       (gameBoard[0][2] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][0])
-    );
+    ) {
+      return players[gameBoard[1][1]];
+    }
   }
 
-  return false;
+  return null;
 }
 
 function App() {
-  // const [activePlayer, setActivePlayer] = useState("X");
-  const [players, setPlayers] = useState({
-    X: "Player 1",
-    O: "Player 2",
-  });
+  const [players, setPlayers] = useState(PLAYERS);
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = [...initialGameBoard.map((array) => [...array])];
+  let gameBoard = deriveGameBoard(gameTurns);
 
-  // iterate over all the turns (won't run if no turns)
-  for (const turn of gameTurns) {
-    // extract info from the turn object
-    const { square, player } = turn;
-    const { row, col } = square;
-    // update the gameboard
-    gameBoard[row][col] = player;
-  }
-
-  let winner;
-
-  if (determineWinner(gameBoard)) {
-    winner = activePlayer == "X" ? players["O"] : players["X"];
-  }
+  let winner = deriveWinner(gameBoard, players);
 
   const hasDraw = gameTurns.length === 9 && !winner;
 
@@ -119,13 +124,13 @@ function App() {
         <div id="game-container">
           <ol id="players" className="highlight-player">
             <Player
-              initialName="Player 1"
+              initialName={PLAYERS.X}
               symbol={"X"}
               onChangeName={handlePlayerNameChange}
               isActive={activePlayer === "X"}
             />
             <Player
-              initialName={"Player 2"}
+              initialName={PLAYERS.O}
               symbol={"O"}
               onChangeName={handlePlayerNameChange}
               isActive={activePlayer === "0"}
